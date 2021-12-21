c242591fc823797f8635798e7fc6bb9e71a35bae
Edgar <ecarrillo046@gmail.com>
2021-12-20 17:22:16 -0800

    feat: add containers holding the text content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,30 @@
 import ShopNowBtn from '../components/buttons/ShopNowBtn';
 import Navbar from '../components/Navbar';
-import '../styles/Home.css';
 
 const Home = () => {
   return (
     <div className="home-page">
       <Navbar />
       <div className="home-background">
-        <p className="home-call-to-action">
-          Are you <br />
-          ready for an <br />
-          adventure?
-        </p>
-        <ShopNowBtn />
+        <div className="home-content-container">
+          <div className="home-call-to-action-content">
+            <p className="home-call-to-action">
+              Are you <br />
+              ready for an <br />
+              adventure?
+            </p>
+            <ShopNowBtn />
+          </div>
+          <div className="home-aside-text-container">
+            <p className="home-aside-text">
+              <span className="small-emphasis-text">
+                UNLOCK YOUR RIDING POTENTIAL
+              </span>
+              <br />
+              Proudly serving the motorcycle community since 2010
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
