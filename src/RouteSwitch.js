d5be4314eb648dd384686bea61b077313813c677
Edgar <ecarrillo046@gmail.com>
2021-12-20 22:19:19 -0800

    feat: import contact page to route

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Contact from './Pages/Contact';
 import Home from './Pages/Home';
 
 const RouteSwitch = () => {
@@ -6,6 +7,7 @@ const RouteSwitch = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
