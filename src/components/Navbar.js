2ac4b64d24384183bc1b9c24755642affa7aad56
Edgar <ecarrillo046@gmail.com>
2021-12-20 17:22:41 -0800

    feat: nest all elements within the container div

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,19 @@ import cartSrc from '../images/cart-white.png';
 
 const Navbar = () => {
   return (
-    <nav>
-      <p className="logo-small">IRON MOTORCYCLES</p>
-      <ul className="nav-items">
-        <li>HOME</li>
-        <li>BIKES</li>
-        <li>CONTACT</li>
-        <li>
-          <img className="img-small" src={cartSrc} alt="Shopping Cart" />
-        </li>
-      </ul>
-    </nav>
+    <div className="container">
+      <nav>
+        <p className="logo-small">IRON MOTORCYCLES</p>
+        <ul className="nav-items">
+          <li>HOME</li>
+          <li>BIKES</li>
+          <li>CONTACT</li>
+          <li>
+            <img className="img-small" src={cartSrc} alt="Shopping Cart" />
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RouteSwitch from './RouteSwitch';
+import './styles/reset.css';
+import './styles/Home.css';
 import './styles/components.css';
 import './styles/index.css';
-import './styles/reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
