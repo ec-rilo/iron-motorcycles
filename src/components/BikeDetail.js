b7ac4f38d582f0f05395b7c2708f8e76d7143f3f
Edgar <ecarrillo046@gmail.com>
2021-12-22 12:15:22 -0800

    feat: modify code to add details for desktop and larger displays

--- src/components/BikeDetail.js
+++ src/components/BikeDetail.js
@@ -4,6 +4,7 @@ import AddToCartBtn from './buttons/AddToCartBtn';
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import uniqid from 'uniqid';
+import { useMediaQuery } from 'react-responsive';
 
 const BikeDetail = () => {
   const location = useLocation();
@@ -13,16 +14,39 @@ const BikeDetail = () => {
     { type: 'ENGINE TORQUE', data: bikeData['engine torque'] },
     { type: 'FUEL ECONOMY', data: bikeData['fuel economy'] },
   ]);
+
+  const isSmallScreen = useMediaQuery({ maxWidth: 1400 });
+
   return (
     <div className="bike-detail-container">
       <Navbar />
-      <div className="bike-detail-upper-content">
-        <div className="bike-detail-info">
-          <p className="bike-detail-category-name">{bikeData.type}</p>
-          <p className="bike-detail-title">{bikeData.name}</p>
+      <div className="bike-detail-upper-content-container">
+        <div className="bike-detail-upper-content">
+          <div className="bike-detail-info">
+            <div className="bike-detail-title-info">
+              <p className="bike-detail-category-name">{bikeData.type}</p>
+              <p className="bike-detail-title">{bikeData.name}</p>
+            </div>
+            <p className="bike-detail-upper-text">{bikeData.description}</p>
+            <p className="bike-detail-upper-price">{`$${bikeData.price}`}</p>
+          </div>
+          <div className="bike-detail-upper-specs">
+            {bikeSpecs.map((specs) => {
+              return (
+                <div key={uniqid()}>
+                  <p className="bike-detail-upper-specs-title">{specs.type}</p>
+                  <p className="bike-detail-upper-specs-text">{specs.data}</p>
+                </div>
+              );
+            })}
+          </div>
+          <img className="bike-detail-img" src={bikeData.image} alt="asef" />
         </div>
-        <img className="bike-detail-img" src={bikeData.image} alt="asef" />
+        <div className="bike-detail-upper-content-lower-contaier">
+          <AddToCartBtn />
+        </div>
       </div>
+
       <div className="bike-detail-lower-content">
         <AddToCartBtn />
         <p className="bike-detail-text">
