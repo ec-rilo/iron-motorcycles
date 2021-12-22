93fe5b1917bf0e14b972299a35f83376af78803d
Edgar <ecarrillo046@gmail.com>
2021-12-22 09:22:37 -0800

    feat: add a path to bike detail page

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BikeDetail from './components/BikeDetail';
 import Bikes from './Pages/Bikes';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home';
@@ -10,6 +11,7 @@ const RouteSwitch = () => {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/bikes" element={<Bikes />} />
+        <Route path="/bikes/:bike_detail" element={<BikeDetail />} />
       </Routes>
     </BrowserRouter>
   );
