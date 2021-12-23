2152d19cfecd09ca41d21df294bcf89fca2f1d4a
Edgar <ecarrillo046@gmail.com>
2021-12-23 10:23:05 -0800

    feat: use props to change the background of the Collection page

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,22 +1,28 @@
+import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 
 const Collection = () => {
+  const location = useLocation();
+  const { data } = location.state;
+  const gradient = 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),';
+  const bgSize = ' center/cover ';
+  const imgSrc = `url(${data.image})`;
+  const coverData = gradient + bgSize + imgSrc;
   return (
     <div className="collection-container">
       <Navbar />
       <div className="collection-landing-page">
-        <div className="collection-landing-cover-container">
-          <p className="collection-landing-cover-title">Cruiser</p>
+        <div
+          style={{ background: coverData, backgroundSize: 'cover' }}
+          className="collection-landing-cover-container"
+        >
+          <p className="collection-landing-cover-title">{data.type}</p>
         </div>
         <div className="collection-landing-data-container">
           <div className="collection-landing-data-text-container">
-            <p className="collection-landing-cover-title">Cruiser</p>
+            <p className="collection-landing-cover-title">{data.type}</p>
             <p className="collection-landing-data-title">2021 Motorcycles</p>
-            <p className="collection-landing-data-text">
-              Authentic heritage meets modern technology for power, style, and
-              the unadulterated riding experience - around town or along country
-              roads.
-            </p>
+            <p className="collection-landing-data-text">{data.description}</p>
           </div>
           <p className="shop-now-text">SHOP NOW</p>
         </div>
