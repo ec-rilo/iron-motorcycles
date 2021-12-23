62fdd6136a4c7a12e5545009b725ace0f1fb9722
Edgar <ecarrillo046@gmail.com>
2021-12-23 10:24:25 -0800

    feat: add react-router Link to view collection button

--- src/components/buttons/ViewCollectionBtn.js
+++ src/components/buttons/ViewCollectionBtn.js
@@ -1,8 +1,16 @@
-const ViewCollectionBtn = () => {
+import { Link } from 'react-router-dom';
+
+const ViewCollectionBtn = (props) => {
+  const { bikes, data } = props;
   return (
-    <div className="view-collection-btn">
+    <Link
+      to={`/bikes/${data.type}_collection`}
+      style={{ textDecoration: 'none' }}
+      state={{ data: data, bikes: bikes }}
+      className="view-collection-btn"
+    >
       <p>View Collection</p>
-    </div>
+    </Link>
   );
 };
 
