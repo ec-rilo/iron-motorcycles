52f42884472047b906e1dc693ce83afd121629ea
Edgar <ecarrillo046@gmail.com>
2021-12-24 10:43:34 -0800

    feat: add togglecartpopup function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,15 @@
 import cartSrc from '../../images/cart-white.png';
 
 const Cart = () => {
+  const toggleCartPopup = () => {
+    const fadedBg = document.querySelector('.faded-bg');
+    fadedBg.classList.toggle('faded-bg-active');
+    const cartPopup = document.querySelector('.cart-popup-container');
+    cartPopup.classList.toggle('cart-popup-active');
+  };
+
   return (
-    <div className="nav-cart-container">
+    <div className="nav-cart-container" onClick={() => toggleCartPopup()}>
       <div className="cart-bubble">
         <p>15</p>
       </div>
