6eb794343e419e30bc2830f80935127a3e8cad17
Edgar <ecarrillo046@gmail.com>
2021-12-25 15:24:12 -0800

    Feat: add populated version of cartpopup

--- src/components/Cart/CartPopup.js
+++ src/components/Cart/CartPopup.js
@@ -1,6 +1,7 @@
 import closeBtnImg from '../../images/icons/close-btn.png';
 import emptyCartImg from '../../images/cart-grey.png';
 import uniqid from 'uniqid';
+import CartPopupSubtotalBtn from '../buttons/CartPopupSubtotalBtn';
 
 const CartPopup = (props) => {
   const { cart } = props;
@@ -47,33 +48,37 @@ const CartPopup = (props) => {
               </div>
             </div>
           ) : (
-            <div className="cart-popup-items-container">
-              {cart.map((bike) => {
-                return (
-                  <div key={uniqid()} className="cart-popup-item">
-                    <img
-                      src={bike.image}
-                      alt={bike.name}
-                      className="cart-popup-bike-img"
-                    />
-                    <div className="cart-popup-item-content">
-                      <p className="cart-popup-title">{bike.type}</p>
-                      <p className="cart-popup-name">{bike.name}</p>
-                      <div className="cart-popup-qty-container">
-                        <p>–</p>
-                        <div className="cart-popup-qty">
-                          <p>2</p>
+            <div>
+              <div className="cart-popup-items-container">
+                {cart.map((bike) => {
+                  return (
+                    <div key={uniqid()} className="cart-popup-item">
+                      <img
+                        src={bike.image}
+                        alt={bike.name}
+                        className="cart-popup-bike-img"
+                      />
+                      <div className="cart-popup-item-content">
+                        <p className="cart-popup-title">{bike.type}</p>
+                        <p className="cart-popup-name">{bike.name}</p>
+                        <div className="cart-popup-qty-container">
+                          <p>–</p>
+                          <div className="cart-popup-qty">
+                            <p>2</p>
+                          </div>
+                          <p>+</p>
                         </div>
-                        <p>+</p>
+                        <p className="cart-popup-price">{`$${bike.price}.00`}</p>
                       </div>
-                      <p className="cart-popup-price">{`$${bike.price}.00`}</p>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
+              <div className="horizontal-line"></div>
+              <CartPopupSubtotalBtn cart={cart} />
             </div>
           )}
-          <div className="horizontal-line"></div>
+          <div className="proceed-to-checkout-btn"></div>
         </div>
       </div>
     </div>
