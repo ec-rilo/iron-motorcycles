2ec99f74ba40eaa6a30921927f180906392961fb
Edgar <ecarrillo046@gmail.com>
2021-12-25 15:25:03 -0800

    feat: create CartPopupSubtotalBtn component

---
+++ src/components/buttons/CartPopupSubtotalBtn.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+
+const CartPopupSubtotalBtn = (props) => {
+  const { cart } = props;
+
+  const [total, setTotal] = useState(0.0);
+
+  /* 'n' is the number you wanted rounded
+   * 'digits' is the number of digits you want rounded to
+   */
+  const roundTo = (n, digits) => {
+    var negative = false;
+    if (digits === undefined) {
+      digits = 0;
+    }
+    if (n < 0) {
+      negative = true;
+      n = n * -1;
+    }
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    n = (Math.round(n) / multiplicator).toFixed(digits);
+    if (negative) {
+      n = (n * -1).toFixed(digits);
+    }
+    return n;
+  };
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  useEffect(() => {
+    let newTotal = 0.0;
+
+    cart.forEach((bike) => {
+      let price = bike.price;
+      price = price.replace(',', '');
+      price = roundTo(price, 2);
+      price = parseFloat(price);
+
+      newTotal += price;
+    });
+
+    newTotal = numberWithCommas(newTotal);
+
+    setTotal(newTotal);
+  }, [cart]);
+
+  return (
+    <div className="cart-popup-subtotal">
+      <p className="subtotal-price">
+        <span className="subtotal-title">Subtotal:</span>{' '}
+        {`$${total.toString()}.00`}
+      </p>
+      <div className="orange-btn subtotal-btn">
+        <p>Proceed to Checkout</p>
+      </div>
+    </div>
+  );
+};
+
+export default CartPopupSubtotalBtn;
