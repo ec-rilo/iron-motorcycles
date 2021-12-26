cf3c69611fcd80b5e937c6446fa19fa49bb0d1d0
Edgar <ecarrillo046@gmail.com>
2021-12-26 11:53:37 -0800

    feat: remove roundTo, numberWithCommas import getSubtotal

--- src/components/buttons/CartPopupSubtotalBtn.js
+++ src/components/buttons/CartPopupSubtotalBtn.js
@@ -1,51 +1,15 @@
 import { useEffect, useState } from 'react';
+import { getSubtotal } from '../../data/math-logic';
 
 const CartPopupSubtotalBtn = (props) => {
   const { cart } = props;
 
   const [total, setTotal] = useState(0.0);
 
-  /* 'n' is the number you wanted rounded
-   * 'digits' is the number of digits you want rounded to
-   */
-  const roundTo = (n, digits) => {
-    var negative = false;
-    if (digits === undefined) {
-      digits = 0;
-    }
-    if (n < 0) {
-      negative = true;
-      n = n * -1;
-    }
-    var multiplicator = Math.pow(10, digits);
-    n = parseFloat((n * multiplicator).toFixed(11));
-    n = (Math.round(n) / multiplicator).toFixed(digits);
-    if (negative) {
-      n = (n * -1).toFixed(digits);
-    }
-    return n;
-  };
-
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  }
-
   useEffect(() => {
-    let newTotal = 0.0;
+    const subTotal = getSubtotal(cart);
 
-    cart.forEach((bike) => {
-      let price = bike.price;
-      price = price.replace(',', '');
-      price = roundTo(price, 2);
-      price = parseFloat(price);
-      price = price * bike.qty;
-
-      newTotal += price;
-    });
-
-    newTotal = numberWithCommas(newTotal);
-
-    setTotal(newTotal);
+    setTotal(subTotal);
   }, [cart]);
 
   return (
